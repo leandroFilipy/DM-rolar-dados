a1f42b8c15500f148b6b33b1383f3a77602319e4
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-02-18 15:15:50 -0300

    feat: adiciona tudo

---
+++ app/src/main/java/com/example/leandromobile/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.leandromobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.leandromobile.ui.theme.LeandroMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LeandroMobileTheme {
+                jogoDoDado(modifier = Modifier.fillMaxSize());
+            }
+        }
+    }
+
+
+}
+@Composable
+fun jogoDoDado(modifier: Modifier = Modifier){
+
+    var result by remember {mutableStateOf(1); }
+    var imageResult = when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(imageResult), contentDescription ="Dado"
+        )
+        Spacer(
+            modifier = Modifier.height(20.dp)
+        )
+        
+        Button(onClick = {
+            result = (1 .. 6).random()
+        }) {
+            Text(text = stringResource(id = R.string.rolar))
+        }
+
+    }
+}
